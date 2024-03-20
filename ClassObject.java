cb822624fc9f833cfb7f0330275828eefb0d308f
Diksha kore <158566433+Dikshakore12@users.noreply.github.com>
2024-03-20 21:43:03 +0530

    Create ClassObject.java

---
+++ ClassObject.java
@@ -0,0 +1,28 @@
+//practical01
+//create class
+package kore;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Diksha");
+		System.out.println("From 4th sem CSE");
+
+	}
+}
+//create object
+package kore;
+
+public class Datatype {
+	int id;
+	String name;
+}
+class Software{
+	public static void main(String[] args) {
+		Datatype d1=new Datatype();
+		d1.id=220400300;
+		d1.name="Diksha Kore";
+		System.out.println("Name:"+d1.name+" Id:"+d1.id);
+	}
+}
+
